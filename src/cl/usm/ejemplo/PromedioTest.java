5cafcc11b8dc7a383a5f113bdabd82e281cec09d
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-11-26 12:14:10 -0300

    Prueba para clases de equivalencia y valores limite

--- src/cl/usm/ejemplo/PromedioTest.java
+++ src/cl/usm/ejemplo/PromedioTest.java
@@ -72,10 +72,71 @@ public class PromedioTest {
 		// valor[1]<=maximo
 		int[] valor = {0,6,-999};
 		int esperado = 6;
-		//valores para que funcione invocación de método.
 		int minimo = 5;
 		int maximo = 10;
 		int actual = promedio.media(valor, minimo, maximo);
 		assertEquals(esperado, actual);
 	}
+	
+	@Test
+	public void testMediaCamino6ClaseFuera1() {
+		// entrada valor[1] <> -999, valor[2] = -999, esperado =-999 (1,2,3,9,10/11,12)
+		// valor[1]>=minimo, maximo = 5
+		// valor[1]<=maximo
+		int[] valor = {0,-3,-999};
+		int esperado = -999;
+		int minimo = 0;
+		int maximo = 100;
+		int actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+	}
+	
+	@Test
+	public void testMediaCamino6ClaseFuera2() {
+		// entrada valor[1] <> -999, valor[2] = -999, esperado =-999 (1,2,3,9,10/11,12)
+		// valor[1]>=minimo, maximo = 5
+		// valor[1]<=maximo
+		int[] valor = {0,110,-999};
+		int esperado = -999;
+		int minimo = 0;
+		int maximo = 100;
+		int actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+	}
+	
+	@Test
+	public void testMediaValorFrontera() {
+		int minimo = 0;
+		int maximo = 100;
+		int[] valor = {0,-1,-999};
+		int esperado = -999;
+		int actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+		
+		valor[1] = 0 ;
+		esperado = 0;
+		actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+		
+		valor[1] = 1 ;
+		esperado = 1;
+		actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+		
+		valor[1] = 99 ;
+		esperado = 99;
+		actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+		
+		valor[1] = 100 ;
+		esperado = 100;
+		actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+		
+		valor[1] = 101 ;
+		esperado = -999;
+		actual = promedio.media(valor, minimo, maximo);
+		assertEquals(esperado, actual);
+	}
+
 }

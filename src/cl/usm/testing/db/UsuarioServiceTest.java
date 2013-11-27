7ad7e9c2e58dc26c208081189a462545dcbb58e8
Marcelo Aliquintuy <mmaliq@gmail.com>
2013-11-27 12:27:15 -0300

    Test usando mock con stub de metodos

--- src/cl/usm/testing/db/UsuarioServiceTest.java
+++ src/cl/usm/testing/db/UsuarioServiceTest.java
@@ -1,5 +1,7 @@
 package cl.usm.testing.db;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -25,5 +27,24 @@ public class UsuarioServiceTest {
 		String user= "user 1";
 		usuarioService.save(user);
 		Mockito.verify(usuarioDao).getUser(user);
+	}
+	
+	@Test
+	public void testSaveExists() {
+		String user= "user 1";
+		Mockito.when(usuarioDao.getUser(user)).thenReturn(user);
+		boolean actual = usuarioService.save(user);
+		assertEquals(false,actual);
+		Mockito.verify(usuarioDao).getUser(user);
+	}
+	
+	@Test
+	public void testSaveAndCreate() {
+		String user= "user 1";
+		Mockito.when(usuarioDao.getUser(user)).thenReturn(null);
+		boolean actual = usuarioService.save(user);
+		assertEquals(true,actual);
+		Mockito.verify(usuarioDao).getUser(user);
+		Mockito.verify(usuarioDao).createUser(user);
 	}
 }
